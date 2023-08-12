eb1262ec3b0bcca774a725047b8a6250618cf20f
GammaGammaRay <benjamin.peret@gmail.com>
2023-08-12 12:14:50 -0300

    feat: validation

---
+++ src/middlewares/validateAuth.js
@@ -0,0 +1,19 @@
+import { getSessionDB } from "../repositories/auth.repository.js"
+
+export async function validateAuth(req, res, next) {
+  const { authorization } = req.headers
+
+  const token = authorization?.replace("Bearer ", "")
+  if (!authorization || token === "Bearer")
+    return res.status(401).send({ message: "Invalid token" })
+
+  try {
+    const { rowCount } = await selectSessionDB(token)
+    if (rowCount === 0)
+      return res.status(401).send({ message: "Session not found" })
+
+    next()
+  } catch ({ message }) {
+    res.status(500).send(message)
+  }
+}
