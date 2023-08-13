d8ebb9e9b8faeead42332ddc1a491865c3c23c25
GammaGammaRay <benjamin.peret@gmail.com>
2023-08-13 15:17:06 -0300

    fix: user schema

--- src/schemas/user.schemas.js
+++ src/schemas/user.schemas.js
@@ -3,10 +3,8 @@ import Joi from "joi"
 export const userSchema = Joi.object({
   userName: Joi.string().required(),
   email: Joi.string().email().required(),
-  cpf: Joi.string().required(),
+  cpf: Joi.string().length(11).required(),
   tel: Joi.string().required(),
-//   cpf: Joi.number().integer().required().min(10000000000).max(99999999999),
-//   tel: Joi.number().integer().required().min(1000000000).max(9999999999),
   userImage: Joi.string().uri().required(),
   password: Joi.string().min(6).required(),
   confirmPassword: Joi.string().min(6).required(),
